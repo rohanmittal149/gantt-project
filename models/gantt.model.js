72b8ea49e3e502bdce5ba4d72ef8faaba841b19d
rohanmittal159 <rohanmittal159@gmail.com>
2021-03-10 01:38:51 +0530

    middleware files

---
+++ models/gantt.model.js
@@ -0,0 +1,79 @@
+import { Schema, model } from 'mongoose';
+ 
+//Attributes of the Project object
+var projectSchema = new Schema({
+name: {
+type: String,
+required: 'This field is required!'
+},
+startDate: {
+type: Date,
+required: 'This field is required!'
+},
+logo: {
+type: String
+},
+projectManager: {
+type: String
+},
+description: {
+type: String
+}
+});
+model('Project', projectSchema);
+
+var taskSchema = new Schema({
+    name: {
+    type: String,
+    required: 'This field is required!'
+    },
+    owner: {
+    type: Int32Array,
+    required: 'This field is required!'
+    },
+    isSubtask: {
+    type: Boolean
+    },
+    startDate: {
+    type: Date
+    },
+    duration: {
+    type: Date
+    },
+    completionPercentage: {
+    type: Int32Array
+    },
+    order: {
+    type: Int32Array
+    }
+    });
+    
+    model('Task', taskSchema);
+
+    var subTaskSchema = new Schema({
+    name: {
+    type: String,
+    required: 'This field is required!'
+    },
+    owner: {
+    type: Int32Array,
+    required: 'This field is required!'
+    },
+    isSubtask: {
+    type: Boolean
+    },
+    startDate: {
+    type: Date
+    },
+    duration: {
+    type: Date
+    },
+    completionPercentage: {
+    type: Int32Array
+    },
+    order: {
+    type: Int32Array
+    }
+    });
+        
+    model('SubTask', subTaskSchema);
