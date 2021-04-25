e39327b69cd71b6b7adbef90397db177bcb907f2
rohanmittal159 <rohanmittal159@gmail.com>
2021-04-26 03:02:25 +0530

    added action methods

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,15 +2,20 @@ import express from "express";
 import Project from "../models/project";
 const router = express.Router();
 
-// get a list of students from the database
+// get a list of projects from the database
 router.get('/project',function(req,res,next){
     Project.find({}).then(function(projects){
         res.send(projects);
     }).catch(next);
 });
 
-// add a new student to database
-router.post('/projects',function(req,res,next){
+router.get('/project/:id',function(req,res,next){
+    Project.findById(req.params.id).then(function(project){
+        res.send(project);
+    }).catch(next);
+});
+// add a new project to database
+router.post('/project',function(req,res,next){
     Project.create(req.body).then(function(project){
         res.send(project);
     }).catch(next);
